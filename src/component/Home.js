c5ba27cb02625d87a70c347e7d6e5712d928cdc3
Y3CiNe <59936894+Y3CiNe@users.noreply.github.com>
2021-11-23 23:55:11 +0100

    Add files via upload

---
+++ src/component/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome</h1>
+      <img
+        src="https://hackernoon.com/images/lovb2b7g.jpg"
+        className="users"
+        alt="conference icon"
+        width="1530"
+        height="664"
+      ></img>
+    </div>
+  );
+};
+export default Home;
